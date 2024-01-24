3e37c0ee29d88e04feb74f52028e6166b774c55f
koukisaifedinn <koukisaif1996@gmail.com>
2024-01-24 12:10:20 +0100

    assignment1

---
+++ function greet(name) {.js
@@ -0,0 +1,5 @@
+function greet(name) {
+    console.log(`Good day, ${name}!`);
+}
+var personName = "Anakin"; 
+greet(personName);
---
+++ group ex.js
@@ -0,0 +1,7 @@
+for (var year = 1996; year <= 2024; year++) {
+    console.log(year,`JS is awesome!`);
+    if (year %11 === 4){
+        console.log(`Happy decade of JS!`);
+    }
+}
+console.log(`HAPPY 28 years of JAVASCRIPT!`);
