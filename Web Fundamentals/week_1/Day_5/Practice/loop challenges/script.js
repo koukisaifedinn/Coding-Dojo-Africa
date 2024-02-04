df8817b76e1cbf9f84c1df15c26f77113d46fa14
koukisaifedinn <koukisaif1996@gmail.com>
2024-02-04 18:35:31 +0100

    js

---
+++ Web Fundamentals/week_1/Day_5/Practice/loop challenges/script.js
@@ -0,0 +1,25 @@
+//
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+//
+for (let i = 100; i >= 1; i--) {
+    if (i % 2!== 0) {
+        console.log(i);
+    }
+}
+//
+var num = 4;
+for (let i = 0; i <=4; i++) {
+    console.log(num);
+    num -= 1.5;
+}
+//
+var sum = 0;
+for (let i = 1; i <=100; i++) {
+    sum += i;
+}
+console.log(sum);
+//
---
+++ Web Fundamentals/week_2/Day_1/Core/Button Clicker/script.js
@@ -0,0 +1,7 @@
+function changebutton(element){
+    console.log(element.innerHTML)
+    element.innerHTML="Logout"
+}
+function hide(element) {
+    element.remove();
+}
